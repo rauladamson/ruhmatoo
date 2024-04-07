b6a994121e8916f572f49bfa6ab054d06e5e897f
martinj91 <145561511+martinj91@users.noreply.github.com>
2024-04-07 11:59:09 +0300

    Selenium html extract töötab minimaalselt, commit for backup ig

---
+++ src/main/java/pdfsave/SeleniumTest.java
@@ -0,0 +1,40 @@
+package pdfsave;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/java/pdfsave/chromedriver32.exe");
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--disable-web-security", "--disable-site-isolation-trials", "--remote-allow-origins=*"); // Giga hack?
+        WebDriver driver = new ChromeDriver(options);
+
+
+
+        try {
+            driver.get("https://ois2.ut.ee/#/courses/LTAT.03.007/version/fde0bca8-705f-74c9-456e-e68104c23b53/details");
+            Thread.sleep(5000); //hack for now :///
+
+
+
+            WebElement element = driver.findElement(By.cssSelector("ois2-courses-version-grading-info-panel"));
+            WebElement element1 = driver.findElement(By.cssSelector("ois2-courses-version-study-hours-info-panel"));
+
+            String content = element.getText() + element1.getText();
+
+
+            System.out.println("Content: " + content);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+}
