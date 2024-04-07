8f67f8500afc489e96e7bcf0d6f61e0324bb8b8b
rauladam <raul.adamson@ut.ee>
2024-04-07 14:33:40 +0300

    Moved all the project files out of the demo folder due to some issues with intellij and maven. Did minor adjustements and reverted pdfprint and pdfprinttest code to working condition

---
+++ src/main/java/com/ut/pdf/Oppeaine.java
@@ -0,0 +1,39 @@
+package com.ut.pdf;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Oppeaine {
+
+    private String text;
+
+    public Oppeaine(String text) {
+        this.text = text;
+
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return this.getText();
+    }
+
+    public static void main(String[] args) {
+
+
+        try (InputStream pdfStream = new FileInputStream("message.txt")) {
+
+            System.out.println(pdfStream.readAllBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
